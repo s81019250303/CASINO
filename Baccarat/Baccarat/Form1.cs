7533d166bd40a6f36585bc53e0c36c83264b62b1
Chris <super.nenb@gmail.com>
2023-10-28 23:14:25 +0800

    shuffle finish

--- Baccarat/Baccarat/Form1.cs
+++ Baccarat/Baccarat/Form1.cs
@@ -2,7 +2,15 @@ namespace Baccarat
 {
     public partial class Form1 : Form
     {
+        //1. config的參數
+        //2
+
+       
+
         int Deckers = 1;//幾幅撲克牌
+        Card[] Cards = new Card[52* 1];
+        int IndexOfCard = 0; //第幾張牌
+        
         enum CardFlower
         {
             RedHeart,//紅心
@@ -20,33 +28,54 @@ namespace Baccarat
         public Form1()
         {
             InitializeComponent();
-
+            InitialCards(Cards,Deckers);
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Shuffle(Cards, Deckers);
         }
 
-        private void InitialCards()
+        private void InitialCards(Card[] cards,int deckers)
         {
-            Card[] cards = new Card[52];
 
-            int index = 0;
-
-            // 1到13初始化卡牌
-            for (int i = 1; i <= 13; i++)
+            //第幾副porkers
+            for (int i = 0; i < deckers; i++)
             {
-                // 4種花色
-                for (int j = 0; j < 4; j++)
+                // 1到13初始化卡牌
+                for (int j = 1; j <= 13; j++)
                 {
-                    cards[index] = new Card();
-                    cards[index].Number = i;
-                    cards[index].Flower = (CardFlower)j;
-                    index++;
+                    // 4種花色
+                    for (int k = 0; k < 4; k++)
+                    {
+                        cards[IndexOfCard] = new Card();
+                        cards[IndexOfCard].Number = j;
+                        cards[IndexOfCard].Flower = (CardFlower)k;
+                        IndexOfCard++;
+                        
+                    }
                 }
             }
 
+
+
         }//InitialCards()
+
+        private void Shuffle(Card[] cards, int deckers)
+        {
+            Random ran = new Random();
+            int a, b;
+            Card temp;
+            for (int i = 0; i < deckers * 52; i++)
+            {
+                a = ran.Next(51);
+                b = ran.Next(51);
+
+                temp = cards[a];
+                cards[a] = cards[b];
+                cards[b] = temp;
+            }
+        }//Shuffle()
     }
 }
